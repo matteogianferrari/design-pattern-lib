63bde48a8cc36b773c33e4411c365b2bf673684a
matteo.gianferrari <mine.matt88@gmail.com>
2023-06-05 22:55:21 +0200

    Added Doxygen documentation for the abstract factory design pattern.

--- AbstractFactory_v1/BmwFactory.h
+++ AbstractFactory_v1/BmwFactory.h
@@ -1,3 +1,15 @@
+/**
+ * @file        BmwFactory.h
+ * @author      Matteo Gianferrari (https://github.com/matteogianferrari)
+ * 
+ * @brief       This file contains the definition of a concrete class BmwFactory.
+ * 
+ * @version     0.1
+ * @date        2023-06-05
+ * 
+ * @copyright   Copyright (c) 2023
+ */
+
 #ifndef BMWFACTORY_H
 #define BMWFACTORY_H
 
@@ -6,18 +18,51 @@
 #include "BmwSuv.h"
 #include "BmwSportCar.h"
 
+/**
+ * @class   BmwFactory "BmwFactory.h" 
+ * @brief   Concrete class for a Bmw car factory.
+ * 
+ * @details This class derives from the abstract class ICarFactory.
+ *          @n This implementation is thread-safe.
+ * 
+ * @author  Matteo Gianferrari
+ * @date    2023-06-05
+ * @version 0.1
+ */
 class BmwFactory:
     public ICarFactory
 {
 public:
+    /**
+     * @fn      BmwFactory
+     * @brief   Construct a new Bmw Factory object.
+     */
     BmwFactory();
+
+    /**
+     * @fn      ~BmwFactory
+     * @brief   Destroy the Bmw Factory object. 
+     */
     ~BmwFactory() override;
 
+    /**
+     * @fn      makeSuv
+     * @brief   Creates and returns a Bmw suv.
+     * 
+     * @return  ISuv* A pointer to an ISuv object.
+     */
     ISuv* makeSuv(void) override;
+
+    /**
+     * @fn      makeSportCar
+     * @brief   Creates and returns a Bmw sport car.
+     * 
+     * @return  ISportCar* A pointer to a ISportCar object.
+     */
     ISportCar* makeSportCar(void) override;
 
 private:
-    std::mutex _mutex;
+    std::mutex _mutex;  /*Mutex for the thread-safety.*/
 };
 
 #endif  //BMWFACTORY_H
--- AbstractFactory_v1/main.cpp
+++ AbstractFactory_v1/main.cpp
@@ -1,3 +1,30 @@
+/**
+ * @file        main.cpp
+ * @author      Matteo Gianferrari (https://github.com/matteogianferrari)
+ * 
+ * @brief       Concrete application example of the "Abstract Factory" design pattern.
+ * @details     This application simulates a multi-thread enviroment where 2 threads
+ *              @n call the Car "Abstract Factory" and perform actions with the cars.
+ *              @n This example shows one implementation of the "Abstract Factory" design pattern.
+ *              This method uses an abstract class ICarFactory with the derived classes that
+ *              are related to car brands. Every car brand factory can create their version of
+ *              the suv and sport car.
+ * 
+ * @note        Use the Abstract Factory pattern when:
+ *              @n -A system should be independent of how its products are created, composed
+ *              and represented.
+ *              @n -A system should be configured with one of multiple families of products.
+ *              @n -A family of related product objects is designed to be used together,
+ *              and you need to enforce this constraint.
+ *              @n -You want to provide a class library of products, and you want to reveal
+ *              just their interfaces, not their implementations.
+ * 
+ * @version     0.1
+ * @date        2023-06-05
+ * 
+ * @copyright   Copyright (c) 2023
+ */
+
 #include <thread>
 #include "ICar.h"
 #include "ICarFactory.h"
@@ -5,6 +32,15 @@
 #include "MercedesFactory.h"
 
 
+/**
+ * @fn      client1 
+ * @brief   Function that simulates a client that creates an suv and a sport car ad uses then.
+ * 
+ * @details This function uses the Car factory to create the cars of the related brand.
+ *          @n Used to simulate a threaded system.
+ * 
+ * @param   pFactory Pointer to a concrete car factory object.
+ */
 void client1(ICarFactory* pFactory)
 {
     ICar* car1 {pFactory->makeSportCar()};
@@ -20,6 +56,15 @@ void client1(ICarFactory* pFactory)
 }
 
 
+/**
+ * @fn      client2
+ * @brief   Function that simulates a client that creates an suv and a sport car ad uses then.
+ * 
+ * @details This function uses the Car factory to create the cars of the related brand.
+ *          @n Used to simulate a threaded system.
+ * 
+ * @param   pFactory Pointer to a concrete car factory object.
+ */
 void client2(ICarFactory* pFactory)
 {
     ICar* car1 {pFactory->makeSportCar()};
